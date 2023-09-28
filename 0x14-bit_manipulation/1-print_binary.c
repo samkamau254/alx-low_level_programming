08b1bddece9a1d717aaef652334fe7f9f8dc88b3
samkamau254 <samkamau254@outlook.com>
2023-09-28 03:59:39 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,39 @@
 #include "main.h"
 
 /**
+ * custom_putchar - writes the character c to stdout
+ * @character: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int custom_putchar(char character);
+
+/**
  * print_binary - Print the binary representation of an unsigned long int
  * @n: The unsigned long int to print in binary
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
+
 	if (n == 0)
 	{
 		custom_putchar('0');
 		return;
 	}
 
-	if (n >> 1)
-		print_binary(n >> 1);
-
-	custom_putchar((n & 1) + '0');
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && flag == 1)
+			custom_putchar('0');
+		else if ((n & mask) != 0)
+		{
+			custom_putchar('1');
+			flag = 1;
+		}
+		mask >>= 1;
+	}
 }
 
