170fee7376a3fe5ae6c2bd7a14f77ea2a068d198
samkamau254 <samkamau254@outlook.com>
2023-10-30 05:48:34 -0400

    yolo

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads and prints the content of a text file to STDOUT.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: If successful, the number of letters it could read and print.
+ *         If an error occurs, return 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int file_descriptor;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    buffer = malloc(letters);
+    if (buffer == NULL)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return (0);
+    }
+
+    bytes_written = write(STDOUT_FILENO, buffer, (size_t)bytes_read);
+    if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return (0);
+    }
+
+    free(buffer);
+    close(file_descriptor);
+    return (bytes_written);
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* Function prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+#endif /* MAIN_H */
